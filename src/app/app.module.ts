7dc63a2672e6b2104183f624d10c1224f7834bcb
Josh <jdherder@gmail.com>
2017-09-29 19:43:11 -0500

    CLI build out of basic components and basic styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FirebaseService } from './services/firebase.service';
+
 import { AppComponent } from './app.component';
+import { BoardComponent } from './components/board/board.component';
+import { SwimLaneComponent } from './components/swim-lane/swim-lane.component';
+import { BoardHeaderComponent } from './components/board-header/board-header.component';
+import { CommentCardComponent } from './components/comment-card/comment-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    SwimLaneComponent,
+    BoardHeaderComponent,
+    CommentCardComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    FirebaseService
+  ],
   bootstrap: [
     AppComponent
   ]
---
+++ src/app/components/swim-lane/swim-lane.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-swim-lane',
+  templateUrl: './swim-lane.component.html',
+  styleUrls: ['./swim-lane.component.scss']
+})
+export class SwimLaneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
