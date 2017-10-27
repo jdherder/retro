e1a79ea156e974777d4311e1a6f1bdd5bdb5a54c
Josh <jdherder@gmail.com>
2017-10-26 21:18:14 -0500

    Update build script for prod mode.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCXG049cdl-leoeZoXDV-Xf9ZaiuIFLcns',
+    authDomain: 'retro-6145a.firebaseapp.com',
+    databaseURL: 'https://retro-6145a.firebaseio.com',
+    projectId: 'retro-6145a',
+    storageBucket: 'retro-6145a.appspot.com',
+    messagingSenderId: '480173019397'
+  }
 };
